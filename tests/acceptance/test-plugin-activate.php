497bbcfd5e127669c23f516f7a9aeb7d815d6e8f
Zach Wills <zachwills@live.com>
2017-01-27 01:04:17 -0500

    Add post type for logging upon plugin activation.

---
+++ tests/acceptance/test-plugin-activate.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Acceptance tests for things that happen during plugin activation.
+ *
+ * @package Bark
+ */
+
+class PluginActivationTest extends WP_UnitTestCase {
+	/**
+	 * @test
+	 */
+	function barkPostTypeShouldExist() {
+		$this->assertTrue( post_type_exists( 'cdv8_bark' ) );
+	}
+}
