ab00f51c5f7f2f68e307b6d1108d2363f12e1cf3
Zach Wills <zachwills@live.com>
2017-02-05 00:46:07 -0500

    Add meta box for message & context.

--- inc/post-types.php
+++ inc/post-types.php
@@ -108,12 +108,19 @@ function bark_set_admin_column_order() {
 add_filter( 'manage_cdv8_bark_posts_columns' , 'bark_set_admin_column_order' );
 
 function bark_register_meta_boxes() {
-	add_meta_box( 'bark-details', __( 'Details', 'bark' ), 'bark_details_display', 'cdv8_bark' );
+	add_meta_box( 'bark-message', __( 'Message', 'bark' ), 'bark_message_display', 'cdv8_bark' );
+	add_meta_box( 'bark-context', __( 'Context', 'bark' ), 'bark_context_display', 'cdv8_bark' );
 }
 add_action( 'add_meta_boxes', 'bark_register_meta_boxes' );
 
-function bark_details_display( $post ) {
+function bark_context_display( $post ) {
 	global $post;
 	$decoded = json_decode( $post->post_content ); ?>
-	<div style="overflow-x: auto;"><?php krumo( $decoded ); ?></div><?php
+	<div style="overflow-x: auto;"><?php krumo( $decoded->context ); ?></div><?php
+}
+
+function bark_message_display( $post ) {
+	global $post;
+	$decoded = json_decode( $post->post_content ); ?>
+	<div><?php echo esc_html( $decoded->message ); ?></div><?php
 }
