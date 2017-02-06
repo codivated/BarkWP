108c409af5175f3a22087d09770eded35ce1a1b8
Zach Wills <zachwills@live.com>
2017-02-06 12:56:43 -0500

    Update bark test.

--- tests/acceptance/test-bark.php
+++ tests/acceptance/test-bark.php
@@ -17,10 +17,7 @@ class BarkTest extends WP_UnitTestCase {
 	public function barkActionAddsEntryToDatabaseAsBarkPostType() {
 		$message = 'Something went wrong!';
 
-		do_action( 'bark', array(
-			'level' => 'error',
-			'message' => $message,
-		) );
+		do_action( 'bark', $message, 'error' );
 
 		$barks = new WP_Query( array(
 			'post_type' => 'cdv8_bark',
@@ -36,29 +33,20 @@ class BarkTest extends WP_UnitTestCase {
 	 */
 	public function barkRespectsLogLimit() {
 		update_option( 'bark-limit-logs', 2 );
-		do_action( 'bark', array(
-			'level' => 'error',
-			'message' => 'Message here',
-		) );
+		do_action( 'bark', 'Message here', 'error' );
 		$barks = new WP_Query( array(
 			'post_type' => 'cdv8_bark',
 		) );
 		$this->assertCount(1, $barks->posts);
 
-		do_action( 'bark', array(
-			'level' => 'error',
-			'content' => 'Message here',
-		) );
+		do_action( 'bark', 'Message here', 'error' );
 		$barks = new WP_Query( array(
 			'post_type' => 'cdv8_bark',
 		) );
 		$this->assertCount(2, $barks->posts);
 
 		// Should fail.
-		do_action( 'bark', array(
-			'level' => 'error',
-			'content' => 'Message here',
-		) );
+		do_action( 'bark', 'Message here', 'error' );
 
 		$barks = new WP_Query( array(
 			'post_type' => 'cdv8_bark',
